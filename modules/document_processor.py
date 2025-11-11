2a742959eeec1e77c011f0b2e5c4c18e3bf82a00
Vashisht123 <109783546+Vashisht123@users.noreply.github.com>
2025-11-11 15:22:03 +0530

    Update document_processor.py

--- modules/document_processor.py
+++ modules/document_processor.py
@@ -7,7 +7,7 @@ import re
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.schema import Document
+from langchain_core.documents import Document
 
 
 
